83ab84014c3d087efedae8c320db52ee25ba72ac
Eyal Cohen <eyalcohen4.ec@gmail.com>
2022-06-01 13:54:54 +0300

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components/App/App';
+import LogRocket from 'logrocket';
+
+import * as desertlion from '@desertlion/sdk';
+
+LogRocket.init('fuqjol/eyal-cohen');
+desertlion.init({
+  appId: '123',
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
